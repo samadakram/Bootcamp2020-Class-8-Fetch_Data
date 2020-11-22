7261e74a2157cbad937fe25b2bee435898734c0b
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-23 02:21:57 +0500

    Update App.js
    post Api function created:
    in GET methode we get Api from server and in POST methode we post api towards server

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 console.log("Fetch API");
 
-async function getApi(){
+async function getApi() {
     const api = await fetch('https://jsonplaceholder.typicode.com/todos/1');
     console.log(api, "API");
 
@@ -9,4 +9,23 @@ async function getApi(){
 
 }
 
-getApi();
+// getApi();
+
+async function postApi() {
+    const apiPost = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'Bootcamp-2020',
+            body: 'JavaScript',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          }
+    })
+    console.log(apiPost, "API");
+    const json1 = await apiPost.json();
+    console.log(json1);
+}
+
+postApi();

76e04adcf4306c58a81bb4232dcaf57097c8983f
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-23 02:00:12 +0500

    Update App.js
    written in async function and converted in json

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 console.log("Fetch API");
 
-const api = fetch('https://jsonplaceholder.typicode.com/todos/1');
+async function apiFetch(){
+    const api = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(api, "API");
 
-console.log(api, "API");
+    const json = await api.json();
+    console.log(json);
+
+}
+
+apiFetch();

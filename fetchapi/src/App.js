f9d80749ff53f0c8afdd2c2ab0254ae5cde4a6eb
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-24 04:25:20 +0500

    Update App.js
    fetched Gitub Repos through Api

--- fetchapi/src/App.js
+++ fetchapi/src/App.js
@@ -5,12 +5,17 @@ function App() {
 
   const [data, setData] = useState({});
 
-  useEffect( ()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => {
-    setData(json)
-  })
+  useEffect( async () => {
+
+    const response = await fetch("https://api.github.com/users/samadakram/repos");
+    const data = await response.json();
+    console.log(data);
+
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     setData(json)
+    //   })
   }, [])
 
   return (

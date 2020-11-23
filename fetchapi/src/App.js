a7a5df27d810c12e1aaeef9b08e91da2ec319910
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-24 04:12:15 +0500

    Update App.js
    learning how to get Api in react

--- fetchapi/src/App.js
+++ fetchapi/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState({});
+
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => {
+    setData(json)
+  })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World from Abdul Samad! {data.title},
     </div>
   );
 }

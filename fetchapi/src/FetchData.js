63de83fcac40e30652e49107c2b636a3e6808a06
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-24 23:25:12 +0500

    Update FetchData.js

--- fetchapi/src/FetchData.js
+++ fetchapi/src/FetchData.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const FetchData = () => {
+
+    const [todos, setTodos] = useState({});
 
     useEffect( async ()=> {
         const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
@@ -8,12 +10,13 @@ export const FetchData = () => {
 
         const data = await response.json();
         console.log("Data =>", data);
+        setTodos(data);
 
     });
 
     return (
         <div>
-            
+            <h2>Title: {todos.title} </h2>
         </div>
     )
 }

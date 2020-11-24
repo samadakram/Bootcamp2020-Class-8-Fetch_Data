45ae9c4702ec2ece3e92cda2fcd0ab38478c64a4
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-24 22:32:28 +0500

    Update fetchdata.js
    Post data to API

--- fetchdata.js
+++ fetchdata.js
@@ -2,6 +2,8 @@
 //   .then(response => response.json())
 //   .then(json => console.log(json))
 
+
+// Get Data
   async function fetchData() {
       const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
       console.log("Response => ", response);
@@ -10,4 +12,27 @@
       console.log("JSON => ", data);
   }
 
+
+  // Post Data
+
+  async function postData() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+          method: "Post",
+          body: JSON.stringify({
+            title: 'My info',
+            body: 'Data',
+            userId: 1,
+          }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          }
+      });
+
+      console.log("Post response => ", response);
+
+      const data = await response.json();
+      console.log("Post Data => ", data );
+  }
+
   fetchData();
+  postData();
